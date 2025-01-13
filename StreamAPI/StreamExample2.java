4d6f462712ca5cb9c47259e9b6ee8508f28557dc
rahulkumar2022 <rahul.kumarcs22@gmail.com>
2025-01-13 10:19:34 +0530

    comparator

--- StreamAPI/StreamExample2.java
+++ StreamAPI/StreamExample2.java
@@ -1,16 +1,21 @@
 package StreamAPI;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamExample2 {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("DDDD","A","AA","AAA","B","BB","BBB","C","CC","CCC");
+        List<String> list = Arrays.asList("DDDD","A","AA","AAA","DD","B","BB","BBB","C","CC","CCC");
         // Sort as per lisy
         List<String> sortedList = list.stream().sorted((a,b)->a.length()-b.length()).toList();
         List<String> sortedList1 = list.stream().sorted((a,b)->b.length()-a.length()).toList();
+        // Making custom comparator
+        Comparator<String> comp = (a,b)->a.length()-b.length();
+        List<String> sortedList2 = sortedList.stream().sorted(comp).toList();
         System.out.println(sortedList);
         System.out.println(sortedList1);
+        System.out.println(sortedList2);
 
     }
 }
